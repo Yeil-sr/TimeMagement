a3342bb3ff7f1300c70f439d8554f1b0ee29ed2d
Yeil-sr <Blackfootout@gmail.com>
2024-09-05 10:48:26 -0300

    fix file ConfigDB

--- api/ConfigDB.js
+++ api/ConfigDB.js
@@ -2,11 +2,13 @@ const sqlite3 = require('sqlite3').verbose();
 const { open } = require('sqlite'); 
 const path = require('path');
 
-function openDb() {
-  return open({
-    filename: path.join(__dirname, 'BizManager.db'), 
+async function openDb() {
+  const db = await open({
+    filename: path.join('/tmp', 'BizManager.db'), 
     driver: sqlite3.Database 
   });
+  
+  return db;
 }
 
 module.exports = { openDb };
